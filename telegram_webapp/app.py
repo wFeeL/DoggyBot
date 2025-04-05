2f713463e16ff5991525cb78536b269b08cb41fd
danya <danny.glazovs@gmail.com>
2025-04-05 22:17:21 +0300

    update app. after creating postgresql

--- telegram_webapp/app.py
+++ telegram_webapp/app.py
@@ -1,6 +1,6 @@
-import asyncio
 from datetime import datetime
 
+import psycopg2
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_folder='static')
@@ -12,29 +12,43 @@ def index():
 # Получение данных пользователя
 @app.route("/get_user_data/<telegram_id>", methods=["GET"])
 def get_user_data(telegram_id):
-    pass
-    # user_profile = asyncio.run(db.get_user_profile(user_id=telegram_id))
-    # pets = asyncio.run(db.get_pets(user_id=telegram_id, is_multiple=True))
+    pg_dsn="postgres://flask_user:password123@91.239.206.123:29572/flask_db"
+    connection = psycopg2.connect(str(pg_dsn))
 
-    # if user_profile:
-    #     if len(pets) > 0:
-    #         pets = list(map(lambda elem: dict(elem), pets))
-    #         for pet in pets:
-    #             pet['birth_date'] = datetime.fromtimestamp(float(pet["birth_date"])).strftime('%Y-%m-%d')
-    #     else:
-    #         pets = []
-    #
-    #     data = {
-    #         'full_name': user_profile['full_name'],
-    #         'phone_number': user_profile['phone_number'],
-    #         'birth_date': datetime.fromtimestamp(user_profile["birth_date"]).strftime('%Y-%m-%d'),
-    #         'about_me': user_profile['about_me'],
-    #         'pets': pets,
-    #     }
-    #     return jsonify(data)
-    # return jsonify({"data": None})
+    with connection as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM user_profile WHERE user_id = '{telegram_id}'ORDER by full_name ASC")
+            user_profile = get_dict_fetch(cur, cur.fetchall())[0]
+            cur.execute(f"SELECT * FROM pets WHERE user_id = '{telegram_id}' ORDER by name ASC")
+            pets = get_dict_fetch(cur, cur.fetchall())
+    if user_profile:
+        if len(pets) > 0:
+            pets = list(map(lambda elem: dict(elem), pets))
+            for pet in pets:
+                pet['birth_date'] = datetime.fromtimestamp(float(pet["birth_date"])).strftime('%Y-%m-%d')
+        else:
+            pets = []
 
+        data = {
+            'full_name': user_profile['full_name'],
+            'phone_number': user_profile['phone_number'],
+            'birth_date': datetime.fromtimestamp(user_profile["birth_date"]).strftime('%Y-%m-%d'),
+            'about_me': user_profile['about_me'],
+            'pets': pets,
+        }
+        return jsonify(data)
+    return jsonify({"data": None})
+
+
+def get_dict_fetch(cursor, fetch):
+    results = []
+    columns = list(cursor.description)
+    for row in fetch:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
+    return results
 
 if __name__ == "__main__":
     app.run(debug=True)
-
