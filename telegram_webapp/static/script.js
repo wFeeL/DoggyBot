cb16c0f94eb0c06d5f1f451bf89a67fef2f1686a
danya <danny.glazovs@gmail.com>
2025-04-07 19:12:03 +0300

    few changes

--- telegram_webapp/static/script.js
+++ telegram_webapp/static/script.js
@@ -20,8 +20,7 @@ window.addPet = addPet;
 window.removePet = removePet;
 
 async function loadUserData() {
-    // const userId = window.Telegram.WebApp.initDataUnsafe.user.id;
-    const userId = '416966184';
+    const userId = window.Telegram.WebApp.initDataUnsafe.user.id;
     if (!userId) return null;
 
     try {
