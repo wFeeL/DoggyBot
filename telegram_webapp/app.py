62554198d0905e8a2c71db123254330d1abd7fa2
danya <danny.glazovs@gmail.com>
2025-04-09 20:27:58 +0300

    few changes

--- telegram_webapp/app.py
+++ telegram_webapp/app.py
@@ -63,44 +63,46 @@ def handle_webapp_data():
 
 
         # Теперь отправляем ответ через Telegram API
-        answer_url = f"https://api.telegram.org/bot{str(os.environ['BOT_TOKEN'])}/answerWebAppQuery"
+        # answer_url = f"https://api.telegram.org/bot{str(os.environ['BOT_TOKEN'])}/answerWebAppQuery"
 
         # valid_data = asyncio.run(db.validate_user_form_data(form_data))
-        valid_data = form_data
-        user_id = form_data['user']['id']
-        if valid_data:
-            human = valid_data['human']
-            asyncio.run(db.update_user_profile(
-                user_id=user_id, birth_date=str_to_timestamp(human["birth_date"]), full_name=human["full_name"],
-                phone_number=human["phone_number"], about_me=human["about_me"]
-            ))
-            asyncio.run(db.delete_pets(user_id))
-            for pet in valid_data["pets"]:
-                asyncio.run(db.add_pet(
-                    user_id=user_id, birth_date=str_to_timestamp(pet["birth_date"]),
-                    approx_weight=pet["weight"],
-                    name=pet["name"], gender=pet["gender"], pet_type=pet["type"], pet_breed=pet["breed"]
-                ))
+        # valid_data = form_data
+        return jsonify(form_data)
 
-        answer_payload = {
-            "web_app_query_id": query_id,
-            "result": {
-                "type": "article",
-                "id": "id1",
-                "title": "Данные получены!",
-                "input_message_content": {
-                    "message_text": f"Спасибо, {form_data['human']['full_name']}! Мы получили ваши данные."
-                }
-            }
-        }
-
-        response = requests.post(answer_url, json=answer_payload)
-
-        if response.status_code == 200:
-            return jsonify({"ok": True})
-        else:
-            return jsonify({"ok": False, "error": response.text})
-
+    #     user_id = form_data['user']['id']
+    #     if valid_data:
+    #         human = valid_data['human']
+    #         asyncio.run(db.update_user_profile(
+    #             user_id=user_id, birth_date=str_to_timestamp(human["birth_date"]), full_name=human["full_name"],
+    #             phone_number=human["phone_number"], about_me=human["about_me"]
+    #         ))
+    #         asyncio.run(db.delete_pets(user_id))
+    #         for pet in valid_data["pets"]:
+    #             asyncio.run(db.add_pet(
+    #                 user_id=user_id, birth_date=str_to_timestamp(pet["birth_date"]),
+    #                 approx_weight=pet["weight"],
+    #                 name=pet["name"], gender=pet["gender"], pet_type=pet["type"], pet_breed=pet["breed"]
+    #             ))
+    #
+    #     answer_payload = {
+    #         "web_app_query_id": query_id,
+    #         "result": {
+    #             "type": "article",
+    #             "id": "id1",
+    #             "title": "Данные получены!",
+    #             "input_message_content": {
+    #                 "message_text": f"Спасибо, {form_data['human']['full_name']}! Мы получили ваши данные."
+    #             }
+    #         }
+    #     }
+    #
+    #     response = requests.post(answer_url, json=answer_payload)
+    #
+    #     if response.status_code == 200:
+    #         return jsonify({"ok": True})
+    #     else:
+    #         return jsonify({"ok": False, "error": response.text})
+    #
     except Exception as e:
         print("Ошибка обработки:", e)
         return jsonify({"ok": False, "error": str(e)})
