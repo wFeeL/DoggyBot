b0a216e3bbd659d0474ffe32f2cc88df9ac4963f
danya <danny.glazovs@gmail.com>
2025-10-05 21:11:28 +0300

    some changes

--- telegram_webapp/app.py
+++ telegram_webapp/app.py
@@ -106,7 +106,6 @@ def survey():
 def handle_survey_data():
     try:
         content = request.json
-        print(dict(content))
         init_data = content.get("initData")
         survey_data = content.get("surveyData")
 
